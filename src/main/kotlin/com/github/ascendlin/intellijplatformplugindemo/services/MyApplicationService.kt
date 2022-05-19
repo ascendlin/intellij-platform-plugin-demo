227135536601e3b454551e0a84b06d70300c57b1
GitHub Action <action@github.com>
2022-05-19 01:40:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ascendlin/intellijplatformplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ascendlin.intellijplatformplugindemo.services
+
+import com.github.ascendlin.intellijplatformplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
